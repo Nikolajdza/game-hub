576d8abf9610d76cd046babb72b0791be74d3e47
Nikola Buljcik <nikola.buljcik@itengine.rs>
2023-09-29 10:56:08 +0200

    Setup routing

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GameDetailPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default GameDetailPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid, GridItem, HStack, Show } from '@chakra-ui/react';
+import GenreList from '../components/GenreList';
+import GameHeading from '../components/GameHeading';
+import PlatformSelector from '../components/PlatformSelector';
+import SortSelector from '../components/SortSelector';
+import GameGrid from '../components/GameGrid';
+
+function HomePage() {
+    return (
+        <Grid
+            templateAreas={{
+                base: `'main'`,
+                lg: `'aside main'`,
+            }}
+            templateColumns={{
+                base: '1fr',
+                lg: '200px 1fr',
+            }}
+        >
+            <Show above="lg">
+                <GridItem area="aside" paddingX={5}>
+                    <GenreList />
+                </GridItem>
+            </Show>
+            <GridItem area="main">
+                <GameHeading />
+                <HStack spacing={5} marginBottom={5}>
+                    <PlatformSelector />
+                    <SortSelector />
+                </HStack>
+                <GameGrid />
+            </GridItem>
+        </Grid>
+    );
+}
+
+export default HomePage;
